3eecd1142624d64fd8d9f64dde9d1ac5b9355d80
Yeasin <yeasinarafatraihan42511@gmail.com>
2026-03-09 01:28:31 +0600

    all card section end

--- script.js
+++ script.js
@@ -41,3 +41,87 @@ if (form) {
  
 // }
 // loadCards();
+
+// card-section
+
+const cardContainer = document.getElementById("cards-container");
+
+async function loadCards() {
+  const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
+  const data = await res.json();
+  displayCards(data.data);
+
+}
+
+function displayCards(cards) {
+  console.log(cards);
+  cards.forEach((card) => {
+    let borderColor;
+let status;
+
+if (card.status === "open") {
+  borderColor = "border-green-500";
+  status = "./assets/Open-Status.png";
+} else {
+  borderColor = "border-blue-500";
+  status = "./assets/Closed-Status.png";
+}
+  
+
+    const cardElement = document.createElement("div");
+    cardElement.className = "bg-white p-4 shadow w-[256px] h-[273px] border-t-4 " + borderColor + " rounded";
+    cardElement.innerHTML = `
+      <div class="flex justify-between items-center gap-2">
+        <img src="${status}" alt="">
+                <button class="btn btn-active rounded-3xl h-7"> <span
+                        class="text-xs text-yellow-600 font-bold">${card.priority}</span></button>
+            </div>
+
+            <h1 class="font-bold text-lg mt-2">Fix Navigation Menu on Mobile Devices</h1>
+            <p class="text-gray-600 mt-1 line-clamp-2">${card.description}</p>
+
+            <div class="mt-1 justify-between items-center gap-0 flex">
+                <button
+                    class="flex items-center gap-2 btn btn-active rounded-3xl h-7 text-yellow-700 px-4 py-2 rounded text-xs">
+
+                    <img src="./assets/BugDroid.png" class="w-4 h-4">
+
+                    BUG
+
+                </button>
+                
+                <button
+                    class="flex items-center gap-2 btn btn-active bg-amber-100 text-yellow-700 rounded-3xl h-7 px-3 py-2 rounded text-xs">
+
+                    <img src="./assets/Vector.png" class="w-4 h-4">
+
+                    HELP WANTED
+
+                </button>
+
+
+
+            </div>
+            <hr class="border-t border-gray-300 my-2">
+            <p class="text-gray-600 line-clamp-2">#1
+                by john_doe</p>
+            <p class="text-gray-600 line-clamp-2">1/15/2024</p>
+
+
+        </div>
+
+
+    </div>
+    `;
+    cardContainer.appendChild(cardElement);
+
+
+    
+  });
+
+
+}
+
+loadCards();
+
+
